5c43be9373da8bb31895ac38a1b25c5e34e75dd8
Benjamin Burstein <bburstein@steamulo.com>
2024-01-05 17:01:58 +0100

    A fix - installation du package
    tentative webpack.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  presets: [require.resolve('@docusaurus/core/lib/babel/preset'),"@babel/preset-env", "@babel/preset-react"],
-};
+  "presets": ["@babel/preset-env", "@babel/preset-react"]
+}
