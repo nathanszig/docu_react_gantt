0ff3768c649f4175f755072a109b6a7943ae6f14
Benjamin Burstein <bburstein@steamulo.com>
2024-01-05 16:40:43 +0100

    A fix - installation du package
    fuck babel et webpack je pige pas

--- src/pages/Demo.js
+++ src/pages/Demo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-
+import Gantt from "free-react-gantt/src/components/gantt";
+import {fakeData} from "free-react-gantt/src/constants/ganttUtils";
 const Demo = () => {
     return (
         <Layout>
             <main>
-                <h2>Wsh nathan</h2>
-                <h1  style={{ color: 'red' }}>KAAN EST GAY</h1>
+                <h1 className={'demo-title'}>Démo Free React Gantt</h1>
+                <Gantt data={fakeData}/>
             </main>
         </Layout>
     );
